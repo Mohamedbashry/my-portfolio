210ddc068a18cdf1b7084afea9286bb1dc28f206
Mohamed Bashry <mohamedbashry849@gmail.com>
2026-08-18 16:41:10 +0530

    updated image

--- src/pages/AboutSinglePage.tsx
+++ src/pages/AboutSinglePage.tsx
@@ -65,7 +65,7 @@ const AboutSinglePage = () => {
               <img 
                 src="/Bashryimg.jpg" 
                 alt="Mohamed Bashry" 
-                className="w-full h-full object-cover object-[100%_70%]"
+                className="w-full h-full object-cover object-[100%_20%]"
               />
             </div>
           </motion.div>
